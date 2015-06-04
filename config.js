a48b876755512f6b27f73cbed4a1618971fb1f56
Nova <novadev94@gmail.com>
2015-06-04 14:05:28 +0700

    Edit thumbor servers

--- config.js
+++ config.js
@@ -5,11 +5,13 @@ module.exports = {
   DOMAIN: process.env.DOMAIN,
   SIMPLEPRINT_DOMAIN: process.env.SIMPLEPRINT_DOMAIN || 'stage.getsimpleprints.com',
   PROXIES: [
-    'http://sp-thumbor-0.herokuapp.com',
     'http://sp-thumbor-1.herokuapp.com',
     'http://sp-thumbor-2.herokuapp.com',
     'http://sp-thumbor-3.herokuapp.com',
     'http://sp-thumbor-4.herokuapp.com',
     'http://sp-thumbor-5.herokuapp.com',
+    'http://sp-thumbor-6.herokuapp.com',
+    'http://sp-thumbor-7.herokuapp.com',
+    'http://sp-thumbor-8.herokuapp.com'
   ],
 }
